d92410f24212c75ca26842afb96f4310621f6a88
DanPaseltiner <dan@skylight.digital>
2022-12-20 10:24:48 -0500

    Add HAPI FHIR server support.

--- utils/generate_and_load_synthea/generate_and_load_synthea.py
+++ utils/generate_and_load_synthea/generate_and_load_synthea.py
@@ -1,3 +1,4 @@
+from distutils.log import INFO
 import os
 import requests
 import subprocess
@@ -6,12 +7,18 @@ import phdi.fhir.transport
 import pathlib
 import shutil
 import click
+import docker
+from time import sleep
+import polling2
+import requests
+import urllib3
+import logging
 
 
 @click.command()
 @click.option(
     "-sv",
-    "--syntheaVersion",
+    "--synthea_version",
     default="v3.1.1",
     type=str,
     help="The version of Synthea to use",
@@ -26,8 +33,35 @@ import click
     show_default=True,
 )
 @click.option(
+    "-ss",
+    "--start_server",
+    default=False,
+    type=bool,
+    help="Start an instance of the HAPI FHIR server on localhost. By default the latest"
+    " version is used, other versions may be specified with --hapi_version. By default "
+    "the server is started on locahost:8080, other ports may be specified with "
+    "--server_port.",
+    show_default=True,
+)
+@click.option(
+    "-sp",
+    "--server_port",
+    default="8080",
+    type=int,
+    help="The port on localhost where an instance of the HAPI FHIR server will be started.",
+    show_default=True,
+)
+@click.option(
+    "-hv",
+    "--hapi_version",
+    default="latest",
+    type=str,
+    help="The version of the HAPI FHIR server that will be started.",
+    show_default=True,
+)
+@click.option(
     "-fu",
-    "--fhirUrl",
+    "--fhir_url",
     default="http://localhost:8080",
     type=str,
     help="URL of the FHIR server that data is uploaded to",
@@ -38,7 +72,7 @@ import click
 )
 @click.option(
     "-p",
-    "--populationSize",
+    "--population_size",
     default=100,
     type=str,
     help="Synthea patient population size",
@@ -46,7 +80,7 @@ import click
 )
 @click.option(
     "-cs",
-    "--clinicianSeed",
+    "--clinician_seed",
     default=1,
     type=int,
     help="clinician seed",
@@ -60,18 +94,26 @@ import click
     show_default=True,
 )
 def generate_and_load_synthea_data(
-    syntheaversion: str,
+    synthea_version: str,
     upload: bool,
-    fhirurl: str,
+    start_server: bool,
+    server_port: int,
+    hapi_version: str,
+    fhir_url: str,
     seed: int,
-    populationsize: int,
-    clinicianseed: int,
+    population_size: int,
+    clinician_seed: int,
     generate_only_alive_patients: str,
 ):
     """
     Generate and Load Synthea is a simple CLI utility for generating synthetic FHIR data
-    with Synthea and uploading it to a FHIR sever. Specifying the Synthea version and
-    seed values allow for FHIR datasets to be reproduced.
+    with Synthea, and uploading it to a FHIR sever. The Synthea version and
+    seed values are controlled to ensure FHIR dataset reproducibility. These datasets
+    are stored in the HOME/synthea/ directory. If a specific dataset is requested that 
+    already exists it will not be recreated, instead the existing data will be used.
+    If you do not already have a FHIR server running Generate and Load Synthea supports 
+    spinning up an instance of the HAPI FHIR server on localhost with Docker 
+    (see options below).  
 
     """
 
@@ -79,11 +121,11 @@ def generate_and_load_synthea_data(
     synthea_directory = pathlib.Path.home() / "synthea"
     if not synthea_directory.is_dir():
         synthea_directory.mkdir()
-    if not (synthea_directory / syntheaversion).is_dir():
-        (synthea_directory / syntheaversion).mkdir()
+    if not (synthea_directory / synthea_version).is_dir():
+        (synthea_directory / synthea_version).mkdir()
 
     # Download the synthea jar file
-    synthea_directory = synthea_directory / syntheaversion
+    synthea_directory = synthea_directory / synthea_version
     if not (synthea_directory / "synthea-with-dependencies.jar").is_file():
         print("Downloading synthea-with-dependencies.jar to...")
         synthea_url = f"https://github.com/synthetichealth/synthea/releases/download/{syntheaversion}/synthea-with-dependencies.jar"
@@ -96,15 +138,14 @@ def generate_and_load_synthea_data(
     synthea_args = ["java", "-jar", "synthea-with-dependencies.jar"]
     synthea_command_arguments = [
         "-p",
-        str(populationsize),
+        str(population_size),
         "-s",
         str(seed),
         "-cs",
-        str(clinicianseed),
-        f"--generate.only_alive_patients={generate_only_alive_patients}"
+        str(clinician_seed),
+        f"--generate.only_alive_patients={generate_only_alive_patients}",
     ]
     synthea_args.extend(synthea_command_arguments)
-    print(synthea_args)
     dataset_directory = (
         synthea_directory / "datasets" / "".join(synthea_command_arguments)
     )
@@ -126,9 +167,38 @@ def generate_and_load_synthea_data(
         )
         shutil.rmtree(synthea_directory / "output")
 
+    # Start HAPI FHIR server:
+    if start_server is True:
+        docker_client = docker.from_env()
+        docker_client.images.pull(f"hapiproject/hapi:{hapi_version}")
+        hapi_server = docker_client.containers.run(
+            f"hapiproject/hapi:{hapi_version}", ports={"8080": server_port}, detach=True
+        )
+        fhir_url = f"http://localhost:{server_port}"
+
+        print("Waiting for the HAPI FHIR server to start...")
+        # Wait for container to start.
+        while hapi_server.status != "running":
+            sleep(1)
+            hapi_server.reload()
+            continue
+        
+        # Poll the /fhir/metdata endpoint untill a 200 is returned to determine that 
+        # the server is up and running.
+        polling2.poll(
+            lambda: requests.get(fhir_url + "/fhir/metadata").status_code == 200,
+            step=5,
+            ignore_exceptions=(
+                urllib3.exceptions.ProtocolError,
+                requests.exceptions.ConnectionError,
+            ),
+            poll_forever=True,
+        )
+        print(f"HAPI FHIR server started on {fhir_url}.")
+
     # Upload to FHIR server
     if upload is True:
-        print(f"Uploading data to a FHIR server located at {fhirurl}...")
+        print(f"Uploading data to a FHIR server located at {fhir_url}...")
         base_path = dataset_directory / "fhir"
         fhir_files = os.listdir(base_path)
         fhir_files.sort(reverse=True)
@@ -141,7 +211,7 @@ def generate_and_load_synthea_data(
                     print(f"Importing {fhir_filepath}")
                     phdi.fhir.transport.http.http_request_with_reauth(
                         cred_manager=None,
-                        url=f"{fhirurl}/fhir",
+                        url=f"{fhir_url}/fhir",
                         retry_count=3,
                         request_type="POST",
                         allowed_methods="POST",

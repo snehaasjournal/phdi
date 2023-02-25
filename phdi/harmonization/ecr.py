3ee9613c53c89d688472886aa796a978cd3bc6c3
DanPaseltiner <dan@skylight.digital>
2023-02-24 23:41:28 -0500

    Extract RR metadata fields directly with Xpath and the .find() method.

--- phdi/harmonization/ecr.py
+++ phdi/harmonization/ecr.py
@@ -1,4 +1,5 @@
 from lxml import etree
+from io import StringIO
 import json
 
 from pathlib import Path
@@ -6,18 +7,15 @@ from pathlib import Path
 ecr_file_name = "jsonified_CDA_eICR.xml"
 rr_file_name = "jsonified_CDA_RR.xml"
 
-ecr = json.loads(Path("jsonified_CDA_eICR.xml").read_text())
-rr = json.loads(Path("jsonified_CDA_RR.xml").read_text())
+ecr = json.loads(Path(ecr_file_name).read_text())
+rr = json.loads(Path(rr_file_name).read_text())
 
-ecr = etree.fromstring(ecr)
+#ecr = etree.fromstring(ecr)
 rr = etree.fromstring(rr)
-rr_tags = ["templateId", "id", "code", "title", "effectiveTime", "confidentialityCode"]
-rr_tags = ["{urn:hl7-org:v3}" + tag for tag in rr_tags]
-rr_elements = []
-for element in rr:
-    if element.tag in rr_tags:
-        rr_elements.append(element)
-    elif element.tag == "{urn:hl7-org:v3}component":
-        print("temp")
 
-rr[14]
+rr_tags = ["templateId", "id", "code", "title", "effectiveTime", "confidentialityCode"]
+rr_elements = []
+for tag in rr_tags:
+    rr_elements.append(rr.find(f"./{tag}", namespaces=rr.nsmap))
+
+breakpoint()
